ac5f5e8937f407cc767cced08775aa861a203f4f
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2023-06-21 11:18:55 +0530

    Update updatejira.py

--- updatejira.py
+++ updatejira.py
@@ -2,17 +2,17 @@ from jira import JIRA
 from jiraconfluence import Confluence
 
 # Connect to JIRA
-jira = JIRA(server='https://your-jira-instance.com', basic_auth=(os.environ['JIRA_USERNAME'], os.environ['JIRA_PASSWORD']))
+jira = JIRA(server='https://demo-sw.atlassian.net/jira/', basic_auth=(os.environ['JIRA_USERNAME'], os.environ['JIRA_PASSWORD']))
 
 # Connect to Confluence
-confluence = Confluence(url=os.environ['CONFLUENCE_URL'], username=os.environ['JIRA_USERNAME'], password=os.environ['JIRA_PASSWORD'])
+confluence = Confluence(url=os.environ['https://demo-sw.atlassian.net/wiki/spaces/~71202026b01f91e1a3436291135759a5455a0e/pages/393217/PR+requests'], username=os.environ['JIRA_USERNAME'], password=os.environ['JIRA_PASSWORD'])
 
 # Fetch pull request details from the GitHub environment variables
 pr_number = os.getenv('GITHUB_REF').split('/')[-1]
 pr_title = os.getenv('GITHUB_HEAD_REF')
 
 # Update the JIRA Confluence page with the pull request details
-page_id = '123456'  # Replace with the ID of your Confluence page
+page_id = 'AQAG'  # Replace with the ID of your Confluence page
 page = confluence.get_page_by_id(page_id)
 table_content = f"| {pr_number} | {pr_title} |\n"  # Customize the table structure as needed
 new_content = f"{page['body']['storage']['value']}\n{table_content}"
